7923c0a18d9eb8c5761fc1ae55546c7b22714e7b
Gustavo Freze <gustavo.freze@outlook.com>
2025-01-06 21:09:40 -0300

    Release/1.0.1 (#4)

--- src/Internal/Exceptions/PropertyCannotBeDeactivated.php
+++ src/Internal/Exceptions/PropertyCannotBeDeactivated.php
@@ -9,6 +9,7 @@ final class PropertyCannotBeDeactivated extends ImmutableException
     public function __construct(string $key, string $class)
     {
         $template = 'Property <%s> cannot be deactivated in class <%s>.';
+
         parent::__construct(message: $this->withMessage($template, $key, $class));
     }
 }
