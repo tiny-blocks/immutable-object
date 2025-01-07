37cc494435264eb2e9c6bb6883da514e893240f6
Gustavo Freze <gustavo.freze@outlook.com>
2025-01-06 21:07:10 -0300

    fix: Fixes execution of composer scripts and updates dependencies. (#3)

--- src/Internal/Exceptions/CollectionCannotBeChanged.php
+++ src/Internal/Exceptions/CollectionCannotBeChanged.php
@@ -9,6 +9,7 @@ final class CollectionCannotBeChanged extends ImmutableException
     public function __construct(mixed $offset, mixed $value, string $class)
     {
         $template = 'Cannot modify collection element at offset <%s> with value <%s> in class <%s>.';
+
         parent::__construct(message: $this->withMessage($template, $offset, $value, $class));
     }
 }
--- src/Internal/Exceptions/PropertyCannotBeChanged.php
+++ src/Internal/Exceptions/PropertyCannotBeChanged.php
@@ -9,6 +9,7 @@ final class PropertyCannotBeChanged extends ImmutableException
     public function __construct(string $key, mixed $value, string $class)
     {
         $template = 'Property <%s> with value <%s> cannot be changed in class <%s>.';
+
         parent::__construct(message: $this->withMessage($template, $key, $value, $class));
     }
 }
