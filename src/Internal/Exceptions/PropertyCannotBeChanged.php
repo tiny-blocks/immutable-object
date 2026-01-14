54697373dec4766eb40bf57857c28182ed789858
Gustavo Freze <gustavo.freze@outlook.com>
2026-01-14 14:20:21 -0300

    feat: Update PHP version to 8.5, improve CI, Makefile, and exception handling.

--- src/Internal/Exceptions/PropertyCannotBeChanged.php
+++ src/Internal/Exceptions/PropertyCannotBeChanged.php
@@ -6,10 +6,13 @@ namespace TinyBlocks\Immutable\Internal\Exceptions;
 
 final class PropertyCannotBeChanged extends ImmutableException
 {
-    public function __construct(string $key, mixed $value, string $class)
-    {
+    public function __construct(
+        private readonly string $key,
+        private readonly mixed $value,
+        private readonly string $class
+    ) {
         $template = 'Property <%s> with value <%s> cannot be changed in class <%s>.';
 
-        parent::__construct(message: $this->withMessage($template, $key, $value, $class));
+        parent::__construct(message: $this->withMessage($template, $this->key, $this->value, $this->class));
     }
 }
--- src/Internal/Exceptions/PropertyCannotBeDeactivated.php
+++ src/Internal/Exceptions/PropertyCannotBeDeactivated.php
@@ -6,10 +6,10 @@ namespace TinyBlocks\Immutable\Internal\Exceptions;
 
 final class PropertyCannotBeDeactivated extends ImmutableException
 {
-    public function __construct(string $key, string $class)
+    public function __construct(private readonly string $key, private readonly string $class)
     {
         $template = 'Property <%s> cannot be deactivated in class <%s>.';
 
-        parent::__construct(message: $this->withMessage($template, $key, $class));
+        parent::__construct(message: $this->withMessage($template, $this->key, $this->class));
     }
 }
